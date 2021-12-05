7047f0cb734362a4fddc7a03b1787e6c706aadc3
黄麟峰 <791815622@qq.com>
2021-12-05 16:23:50 +0800

    登录页使用异步提交。

--- src/main/java/com/github/linfeng/plan/controller/PlanLoginController.java
+++ src/main/java/com/github/linfeng/plan/controller/PlanLoginController.java
@@ -1,17 +1,17 @@
 package com.github.linfeng.plan.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import com.github.linfeng.admin.view.message.AjaxViewMessage;
 import com.github.linfeng.admin.view.message.MessageType;
-import com.github.linfeng.admin.view.message.ViewMessage;
 import com.github.linfeng.plan.service.IPlanUsersService;
 import com.github.linfeng.plan.view.LoginUser;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 作计划登录
@@ -55,31 +55,26 @@ public class PlanLoginController {
      * @return 成功则进入页, 失败则登录页
      */
     @RequestMapping(value = "doLogin", method = RequestMethod.POST)
-    public String doLogin(HttpServletRequest request, String loginName, String loginPwd, Integer remember,
-        Model model) {
+    @ResponseBody
+    public AjaxViewMessage doLogin(HttpServletRequest request, String loginName, String loginPwd, Integer remember) {
 
-        ViewMessage msg = null;
+        AjaxViewMessage msg = null;
         if (!StringUtils.hasText(loginName)
             || !StringUtils.hasText(loginPwd)) {
-            msg = new ViewMessage(MessageType.ERROR, "请求参数不正确", "用户名或密码为空");
-            model.addAttribute("msg", msg);
-            return "plan/login/login";
+            msg = new AjaxViewMessage(MessageType.ERROR, "请求参数不正确", "用户名或密码为空");
+             return msg;
         }
         if (remember == null) {
             remember = 0;
         }
         LoginUser loginUser = planUsersService.checkLogin(loginName, loginPwd);
         if (loginUser != null && loginUser.getUserId() > 0) {
-            msg = new ViewMessage(MessageType.SUCCESS, "登录成功", "登录成功", "plan/login/into",
+            msg = new AjaxViewMessage(MessageType.SUCCESS, "登录成功", "登录成功", "plan/login/into",
                 request.getContextPath() + "/plan/weeks/index");
             request.getSession().setAttribute("loginUser", loginUser);
         } else {
-            msg = new ViewMessage(MessageType.SUCCESS, "登录失败", "用户名或密码错误", "plan/login/login");
+            msg = new AjaxViewMessage(MessageType.INFO, "登录失败", "用户名或密码错误", "plan/login/login");
         }
-
-        model.addAttribute("remember", remember);
-        model.addAttribute("loginName", loginName);
-        model.addAttribute("msg", msg);
-        return msg.getViewPath();
+        return msg;
     }
 }
