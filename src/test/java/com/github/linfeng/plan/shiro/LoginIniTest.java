6506169e2bc7c11f0f83333ba491b022bd74f409
黄麟峰 <791815622@qq.com>
2021-12-18 09:54:29 +0800

    shiro测试分为JDBC和Ini两个方向。

--- src/test/java/com/github/linfeng/plan/shiro/LoginIniTest.java
+++ src/test/java/com/github/linfeng/plan/shiro/LoginIniTest.java
@@ -2,8 +2,6 @@ package com.github.linfeng.plan.shiro;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
@@ -17,11 +15,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * shiro登录测试
+ * shiro简单存储登录测试
  */
-public class LoginTest {
+public class LoginIniTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoginTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginIniTest.class);
 
     /**
      * 测试用户名密码登录
@@ -48,41 +46,6 @@ public class LoginTest {
         Assert.assertTrue(subject.isAuthenticated());
         // 用户登出
         subject.logout();
-    }
-
-    /**
-     * 测试用户名密码登录,JDBC
-     */
-    @Test
-    public void testJdbcLogin() {
-        Factory<SecurityManager> factory =
-            new IniSecurityManagerFactory("classpath:shiro-jdbc-realm.ini");
-        SecurityManager securityManager = factory.getInstance();
-        SecurityUtils.setSecurityManager(securityManager);
-        Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken("foo", "123456");
-        try {
-            // 登录
-            subject.login(token);
-        } catch (UnknownAccountException notUserEx) {
-            LOGGER.warn("用户不存在.", notUserEx);
-        } catch (IncorrectCredentialsException errorPasswordEx) {
-            LOGGER.warn("密码不正确.", errorPasswordEx);
-        } catch (AuthenticationException e) {
-            LOGGER.warn(e.getMessage(), e);
-        }
-        // 验证登录
-        Assert.assertTrue(subject.isAuthenticated());
-        try {
-            // 验证角色
-            Assert.assertTrue(subject.hasRole("admin"));
-            // 验证权限
-            Assert.assertTrue(subject.isPermitted("plan:create"));
-        } finally {
-            // 登出
-            subject.logout();
-        }
-
     }
 
     @After
