c9508a5d549f484bb49577add3bcfcca22755fe3
黄麟峰 <791815622@qq.com>
2023-01-22 14:31:03 +0800

    在日历中可直接增加任务项.

--- src/main/java/com/github/linfeng/plan/controller/PlanCalendarController.java
+++ src/main/java/com/github/linfeng/plan/controller/PlanCalendarController.java
@@ -3,16 +3,19 @@ package com.github.linfeng.plan.controller;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.alibaba.fastjson.JSON;
 import com.github.linfeng.plan.entity.CalendarEventObject;
 import com.github.linfeng.plan.entity.PlanItems;
+import com.github.linfeng.plan.entity.PlanUsers;
 import com.github.linfeng.plan.holder.LoginUserHolder;
 import com.github.linfeng.plan.service.IPlanItemsService;
 import com.github.linfeng.plan.service.IPlanUsersService;
 import com.github.linfeng.plan.service.IPlanWeeksService;
+import com.github.linfeng.plan.view.JobType;
 import com.github.linfeng.plan.view.LoginUser;
 
 import org.apache.shiro.authz.annotation.RequiresRoles;
@@ -35,7 +38,7 @@ public class PlanCalendarController extends BasePlanController {
 
     @Autowired
     public PlanCalendarController(IPlanWeeksService weeksService, IPlanItemsService itemsService,
-            IPlanUsersService planUsersService) {
+        IPlanUsersService planUsersService) {
         this.weeksService = weeksService;
         this.itemsService = itemsService;
         this.planUsersService = planUsersService;
@@ -46,6 +49,12 @@ public class PlanCalendarController extends BasePlanController {
     public String index(Model model, HttpServletRequest request, HttpServletResponse response) {
         LoginUser loginUser = LoginUserHolder.getLoginUser();
         model.addAttribute("admin", loginUser);
+        Map<String, String> jobTypes = JobType.toMap();
+        model.addAttribute("jobTypes", jobTypes);
+
+        List<PlanUsers> users = planUsersService.list();
+        model.addAttribute("users", users);
+        model.addAttribute("weeks", weeksService.list());
 
         String date = request.getParameter("date");
         if (date == null) {
@@ -73,7 +82,7 @@ public class PlanCalendarController extends BasePlanController {
     @RequestMapping(value = "/feed", produces = "application/json;charset=UTF-8")
     @ResponseBody
     public Object feed(@RequestParam("start") String start, @RequestParam("end") String end, HttpServletRequest request,
-            HttpServletResponse response) {
+        HttpServletResponse response) {
         List<CalendarEventObject> ret = new ArrayList<>();
 
         Long startDate = OffsetDateTime.parse(start).toInstant().getEpochSecond() * 1000;
@@ -84,8 +93,12 @@ public class PlanCalendarController extends BasePlanController {
         List<PlanItems> items = itemsService.list(startDate, endDate);
         for (PlanItems item : items) {
             String color = color(item.getId() % 6);
+            boolean allDay = false;
+            if (item.getEndDate() - item.getBeginDate() > 24 * 60 * 60 * 1000L) {
+                allDay = true;
+            }
             CalendarEventObject evt = new CalendarEventObject(item.getTitle(), item.getBeginDate(), item.getEndDate(),
-                    color, color, false);
+                color, color, allDay);
             evt.setUrl(urlFormat + item.getId());
             ret.add(evt);
         }
@@ -97,11 +110,11 @@ public class PlanCalendarController extends BasePlanController {
 
     private String color(int index) {
         String[] colors = new String[]{"#f56954", //red
-                "#f39c12", //yellow
-                "#0073b7", //Blue
-                "#00c0ef", //Info (aqua)
-                "#00a65a", //Success (green)
-                "#3c8dbc", //Primary (light-blue)
+            "#f39c12", //yellow
+            "#0073b7", //Blue
+            "#00c0ef", //Info (aqua)
+            "#00a65a", //Success (green)
+            "#3c8dbc", //Primary (light-blue)
         };
         if (index < 0 || index >= colors.length) {
             index = random.nextInt(colors.length);
